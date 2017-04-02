18c20397f07045d907cbf2d6ea84ce4cd53ca1e5
robthemonster <robthemonsterlevin@gmail.com>
2017-04-02 14:48:39 -0400

    more ending generator work

--- testSFML/testSFML/EndingGenerator.cpp
+++ testSFML/testSFML/EndingGenerator.cpp
@@ -1,7 +1,45 @@
 #include <vector>
+#include "Requirements.h"
+#include "Path.h"
 #include "EndingGenerator.h"
 
 
+
+void EndingGenerator::setTiers(int tiers)
+{
+	for (int i = 0; i < tiers; i++) {
+		this->endings.push_back(std::vector<std::tuple<Path*, Requirements* > >());
+	}
+}
+
+void EndingGenerator::addEnding(int tier, Path * path, Requirements * req)
+{
+	this->endings[tier].push_back(std::make_tuple(path, req));
+}
+
+void EndingGenerator::display(sf::RenderWindow window, sf::View view)
+{
+	Path * first = NULL, *curr = NULL, *last = NULL;
+	Requirements * currReq;
+	for (int i = 0; i < this->endings.size(); i++) {
+		for (int j = 0; j < this->endings[i].size(); j++) {
+			currReq = std::get<1>(this->endings[i][j]);
+			if ((*currReq).meetsAllRequirements()) {
+				curr = std::get<0>(this->endings[i][j]);
+				if (first == NULL)
+					first = curr;
+				break;
+			}
+		}
+
+		if (last != NULL && curr != NULL) {
+			(*last).setDestination((Navigable**)&curr);
+			last = curr;
+		}
+	}
+
+	(*first).display(window, view);
+}
 
 EndingGenerator::EndingGenerator()
 {
