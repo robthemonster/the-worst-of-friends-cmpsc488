775837337450ef86ba9ae603cb532462e5de2051
gtf5022 <gtf5022@gmail.com>
2017-04-03 11:36:45 -0400

    NPC Class
    Framework for the NPC Class has been added. Currently only has a name attribute.

---
+++ GUI_Test2/GUI_Test2/NPC.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_Test2
+{
+    [Serializable]
+    class NPC
+    {
+        public string name;
+        //public Dictionary<String, String> image;
+
+        public NPC()
+        {
+        }
+
+        public NPC(string n)
+        {
+            name = n;
+        }
+    }
+}
