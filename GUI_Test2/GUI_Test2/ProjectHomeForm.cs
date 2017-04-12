d93d05c3bd5bef68338c3768bb130754d8f5af5e
gtf5022 <gtf5022@gmail.com>
2017-04-11 22:30:50 -0400

    Ending Gen in Game and Project
    Game and Project now have EndingGen variables.
    Old project saves will be incompatible.

--- GUI_Test2/GUI_Test2/ProjectHomeForm.cs
+++ GUI_Test2/GUI_Test2/ProjectHomeForm.cs
@@ -113,7 +113,7 @@ namespace GUI_Test2
             if (fileLocation != "")
             {
                 //Game.init(pathGroups, hubs, navIndex, navigableName, paths);
-                Project proj = new Project(Game.pathGroups, Game.hubs, Game.navIndex, Game.navigableName, Game.paths, Attributes.attribs, Characters.characters);
+                Project proj = new Project(Game.pathGroups, Game.hubs, Game.navIndex, Game.navigableName, Game.paths, Attributes.attribs, Characters.characters, Game.endingGen);
                 saveToFile(proj, fileLocation);
             }
             else
