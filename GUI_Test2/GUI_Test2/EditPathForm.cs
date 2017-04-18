3620e6ccc594f494fdc4a9ada40ae1608396991b
gtf5022 <gtf5022@gmail.com>
2017-04-17 21:52:38 -0400

    Dialogue Load Fix in Edit Path
    Fixed a bug where Dialogue List wasn't updating on first loading of
    a saved path.

--- GUI_Test2/GUI_Test2/EditPathForm.cs
+++ GUI_Test2/GUI_Test2/EditPathForm.cs
@@ -84,6 +84,7 @@ namespace GUI_Test2
 
             defaultTargetNavigable = p.defaultTargetNavigable;
 
+            updateListBoxes();
             updateButtonListBox();
         }
             
@@ -121,8 +122,6 @@ namespace GUI_Test2
             System.Windows.Forms.ToolTip defaultTargetNavToolTip = new System.Windows.Forms.ToolTip();
             string defaultTargetNavMsg = "The default destination of a path \nif no buttons are given to it.";
             defaultTargetNavToolTip.SetToolTip(this.useDefaultTargetNavigableCheckBox, defaultTargetNavMsg);
-
-            updateListBoxes();
         }
 
         private void selectDefaultPathImageButton_Click(object sender, EventArgs e)
