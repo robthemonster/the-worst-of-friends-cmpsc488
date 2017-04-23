081dc6dda7c4f67f36650a7c7209ccc7cadbe21d
gtf5022 <gtf5022@gmail.com>
2017-04-23 19:11:06 -0400

    Minor Visual Update
    Borders on Create Character Form were too small and gave no margin on
    the bottom and right portions of the screen.

--- GUI_Test2/GUI_Test2/EditCharactersForm.Designer.cs
+++ GUI_Test2/GUI_Test2/EditCharactersForm.Designer.cs
@@ -232,7 +232,7 @@
             this.AcceptButton = this.createNewCharacterButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(427, 193);
+            this.ClientSize = new System.Drawing.Size(437, 207);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBox2);
